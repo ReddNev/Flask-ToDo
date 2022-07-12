f7f0a1e8e11a779e2db776262f9869bf35556d6d
ReddNev <creedlll@mail.ru>
2022-07-12 13:30:09 +0300

    [ADD] routes

---
+++ todo/routes.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, url_for, redirect
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
+
+
+app = create_app()
+
+
+@app.get('/')
+def home():
+    pass
+
+
+@app.get('/')
+def add():
+    pass
+
+
+@app.get('/')
+def update():
+    pass
+
+
+@app.get('/')
+def delete():
+    pass
+
+
+
