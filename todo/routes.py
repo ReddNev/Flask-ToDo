77ec7552062da9185a62e26b57e2fa7525b152d2
ReddNev <creedlll@mail.ru>
2022-07-12 14:11:37 +0300

    [ADD] route to add entry

--- todo/routes.py
+++ todo/routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template, url_for, redirect
+from todo.models import ToDo, db
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
     return app
 
 
@@ -13,12 +15,18 @@ app = create_app()
 
 @app.get('/')
 def home():
-    pass
+    todo_list = ToDo.query.all()
+    return render_template('todo/index.html', todo_list=todo_list, title='Main page')
 
 
-@app.get('/')
+@app.post('/add')
 def add():
-    pass
+    title = request.form.get('title')
+    new_todo = ToDo(title=title, is_complate=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 
 @app.get('/')
